3f020fba81982357cc92a9b833912fa6b32b1337
Danilo Kodavara <danilo.kodavara@hotmail.com>
2022-02-19 13:57:39 -0300

    Utilizando pattern matching e case para tratar erro de load

--- lib/cards.ex
+++ lib/cards.ex
@@ -10,30 +10,32 @@ defmodule Cards do
 
   end
 
-  def shuffle(deck) do
-    Enum.shuffle(deck)
+  def shuffle deck do
+    Enum.shuffle deck
   end
 
-  def contains?(deck, card) do
-    Enum.member?(deck, card)
+  def contains? deck, card do
+    Enum.member? deck, card
   end
 
-  def deal(deck, hand_size) do
-    {hand, rest} = Enum.split(deck, hand_size)
+  def deal deck, hand_size do
+    {hand, _rest} = Enum.split deck, hand_size
     hand
   end
 
-  def save(deck, filename) do
-    binary = :erlang.term_to_binary(deck)
-    File.write(filename, binary)
+  def save deck, filename do
+    binary = :erlang.term_to_binary deck
+    File.write filename, binary
   end
 
-  def load(filename) do
-    {status, binary} = File.read(filename)
-
-    case status do
-      :ok -> :erlang.term_to_binary(binary)
-      :error -> "That file does not exist"
+  def load filename do
+    case File.read filename do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _reason} -> "That file does not exist"
     end
+  end
+
+  def create_hand(hand_size) do
+
   end
 end
